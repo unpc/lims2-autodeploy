be8f1db782551a354cdd4c6d39fb3c0c1dd78b1d
Cheng.liu <cheng.liu@geneegroup.com>
2018-01-25 16:00:41 +0800

    add sphinx contain support

--- template/bin/update-database-config.sh
+++ template/bin/update-database-config.sh
@@ -15,3 +15,8 @@ if [ ! -e "$DATABASE_CONFIG_FILE_FMT" ]; then
 fi
 echo "\$config['root'] = 'mysql://genee:83719730@mysql:3306/%database';\n" >> "$DATABASE_CONFIG_FILE_FMT"
 echo "\$config['@sphinx.url'] = 'mysql://sphinx:9306';\n" >> "$DATABASE_CONFIG_FILE_FMT"
+
+GLOBALS_FILE=$DOCKER_LIMS2_DIR/public/globals.php
+if [ -e "$GLOBALS_FILE" ]; then
+	sed -i "s/172.17.42.1/127.0.0.1/g" $GLOBALS_FILE
+fi
