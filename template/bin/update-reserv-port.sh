f0f1f22d5bdfbb9803c775f999b1b75f079b1ae8
unpc <cheng.liu@geneegroup.com>
2018-05-08 17:17:18 +0800

    [Update] Fix bug to use lims2 2.18

--- template/bin/update-reserv-port.sh
+++ template/bin/update-reserv-port.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 echo "\$config['io.address'] = 'http://rum.genee.cn:${RESERV_PUB_PORT}';" >> $DOCKER_LIMS2_DIR/modules/calendars/config/calendar.php
+
+cli_file=$DOCKER_LIMS2_DIR/modules/eq_reserv/libraries/cli
+
+[ ! -w $cli_file ] || sed -i "s/172.17.42.1/127.0.0.1/g" $cli_file/partner.php
+[ ! -w $cli_file ] || sed -i "s/172.17.42.1/127.0.0.1/g" $cli_file/tube.php
+
+config_file=$DOCKER_LIMS2_DIR/modules/eq_reserv/config/beanstalkd.php
+
+[ ! -w $config_file ] || sed -i "s/172.17.42.1/127.0.0.1/g" $config_file
+
+
