bf5fcf98a758204f50a578bb26b0b3b61fc530ba
Cheng.Liu <cheng.liu@geneegroup.com>
2018-12-12 14:33:11 +0800

    gdoor功能

--- template/bin/update-gdoor-port.sh
+++ template/bin/update-gdoor-port.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 echo "\$config['dc_gdoor']['port'] = ${GDOOR_PUB_PORT}; // gdoor 门禁端口" >> $DOCKER_LIMS2_DIR/modules/entrance/config/device.php
+
+GDOOR_CONFIG_FILE=$DOCKER_GDOOR_SERVER_DIR/config/default.js
+if [ -e "$GDOOR_CONFIG_FILE" ]; then
+	sed -i "s/172.17.42.1/127.0.0.1/g" $GDOOR_CONFIG_FILE
+fi
+
+if [ -e "$GDOOR_CONFIG_FILE" ]; then
+	sed -i "s/2950/${GDOOR_API_PUB_PORT}/g" $GDOOR_CONFIG_FILE
+fi
+
