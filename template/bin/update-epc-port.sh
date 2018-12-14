1143f2a8c6eef9db29be1ae0c766e43ed955f677
Cheng.Liu <cheng.liu@geneegroup.com>
2018-12-14 14:19:56 +0800

    更新gdoor几只

---
+++ template/bin/update-epc-port.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 对epc代码进行再次更新
+cd $DOCKER_EPC_SERVER_DIR 
+git remote set-url origin git@192.168.0.26:epc-server.git
+git stash --include-untracked
+git pull origin develop
+git stash pop
+
+EPC_CONFIG_FILE=$DOCKER_EPC_SERVER_DIR/config/default.js
+if [ -e "$EPC_CONFIG_FILE" ]; then
+	sed -i "s/172.17.42.1/127.0.0.1/g" $EPC_CONFIG_FILE
+fi
+
+if [ -e "$GDOOR_CONFIG_FILE" ]; then
+	sed -i "s/3061/3061, host:'http:\/\/127.0.0.1'/g" $GDOOR_CONFIG_FILE
+fi
+
+supervisorctl restart epc-server
